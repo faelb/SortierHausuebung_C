a84a5a36f1b85f13c1a1fe7810cb7e7bdda121c9
Christoph Felber <faelb@gmx.at>
2019-06-16 14:18:15 +0200

    push first files

---
+++ cmake-build-debug/CMakeFiles/SortierHausuebung_C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SortierHausuebung_C.dir/main.c.o"
+  "SortierHausuebung_C.pdb"
+  "SortierHausuebung_C"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SortierHausuebung_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
